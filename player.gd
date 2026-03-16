34e7aee62b86494e315ea62c93fc64a1cf60a869
Imperialcereal5 <hthompson.bcop@gmail.com>
2026-03-16 22:37:34 +0000

    started setting up maps

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 @onready var feet: ShapeCast2D = $ShapeCast2D
 var speed = 100
-var jumpPwr = 100
+var jumpPwr = 250
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,8 +14,7 @@ func _physics_process(delta:float) -> void:
 	var xMotion = 0
 	if feet.is_colliding():
 		if Input.is_action_pressed("jump"):
-			velocity.y = jumpPwr
-		#print("grounded")
+			velocity.y = -jumpPwr
 	else:
 		velocity.y += 9.81
 	if Input.is_action_pressed("left"):
--- side_on.gd
+++ side_on.gd
@@ -1,13 +1,27 @@
 extends Node2D
+class Zone:
+	var area:String
+	var maxCoords:Vector2
+	var minCoords:Vector2 
+	var map:TileMapLayer
+	func _init(a:String, max:Vector2, min:Vector2, m:TileMapLayer):
+		area = a
+		maxCoords = max
+		minCoords = min
+		map = m
 @onready var levelmaps: Node2D = $levelmaps
-@onready var zones = [{"area":"pixel", "maxCoords":Vector2(0,0), "minCoords":Vector2(0,0)}]
-var maps = []
+var zones = []
+var coords = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in range(len(zones)):
-		#maps[i] = levelmaps.get_child(i)
-		pass
+	for i in range(len(coords)):
+		if i < 10:
+			zones.append(Zone.new("pixel",Vector2(coords[i][0],coords[i][1]),Vector2(coords[i][2],coords[i][3]),levelmaps.get_child(i)))
+		elif i < 20:
+			zones.append(Zone.new("digital",Vector2(coords[i][0],coords[i][1]),Vector2(coords[i][2],coords[i][3]),levelmaps.get_child(i)))
+		else:
+			zones.append(Zone.new("trad",Vector2(coords[i][0],coords[i][1]),Vector2(coords[i][2],coords[i][3]),levelmaps.get_child(i)))
 		
 
 
