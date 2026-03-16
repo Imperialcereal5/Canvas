07abeb9a1e0074c4fe559c1d0f0843b13bc7703a
Imperialcereal5 <hthompson.bcop@gmail.com>
2026-03-16 19:58:10 +0000

    poopoopeepee

--- 2d_cam.gd
+++ 2d_cam.gd
@@ -1,5 +1,5 @@
 extends Camera2D
-@onready var player: RigidBody2D = $"../Player"
+@onready var player: CharacterBody2D = $"../Player"
 @onready var bg_front: Sprite2D = $bgFront
 @onready var bg_back: Sprite2D = $bgBack
 @onready var initPos = position
@@ -8,7 +8,7 @@ extends Camera2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.x = player.position.x
--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
-extends RigidBody2D
-var speed = 2
-
+extends CharacterBody2D
+@onready var feet: ShapeCast2D = $ShapeCast2D
+var speed = 100
+var jumpPwr = 100
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,10 +10,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-func _physics_process(_delta:float) -> void:
-	var motion = Vector2(0, 0)
+func _physics_process(delta:float) -> void:
+	var xMotion = 0
+	if feet.is_colliding():
+		if Input.is_action_pressed("jump"):
+			velocity.y = jumpPwr
+		#print("grounded")
+	else:
+		velocity.y += 9.81
 	if Input.is_action_pressed("left"):
-		motion.x -= speed
+		xMotion -= speed
 	if Input.is_action_pressed("right"):
-		motion.x += speed
-	move_and_collide(motion)
+		xMotion += speed
+	velocity.x = xMotion
+	move_and_slide()
